75de7e9d872df8a5854293b8cb6974d4a3becef8
ajitjcopilot <ajitjaiswal@gmail.com>
2024-07-02 11:32:05 +0530

    Added test cases

--- src/main/java/com/scaler/bookmyshow/controllers/ShowController.java
+++ src/main/java/com/scaler/bookmyshow/controllers/ShowController.java
@@ -4,6 +4,8 @@ import com.scaler.bookmyshow.models.Language;
 import com.scaler.bookmyshow.models.SeatType;
 import com.scaler.bookmyshow.models.Show;
 import com.scaler.bookmyshow.services.ShowService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -13,7 +15,8 @@ import java.util.Map;
 @Controller
 public class ShowController {
 
-    private ShowService showService;
+    private final ShowService showService;
+    private static final Logger logger = LoggerFactory.getLogger(ShowController.class);
 
     @Autowired
     public ShowController(ShowService showService) {
@@ -28,8 +31,11 @@ public class ShowController {
             Map<SeatType, Integer> seatPricing,
             Language language
     ) {
-        return showService.createShow(
+        logger.info("Creating show for movieId: {}, auditoriumId: {}, language: {}, startTime: {}, endTime: {}",
+                movieId, auditoriumId, language, startTime, endTime);
+        Show show = showService.createShow(
                 movieId, startTime, endTime, auditoriumId, seatPricing, language);
+        logger.info("Show created successfully with id: {}", show.getId());
+        return show;
     }
-
 }
--- src/main/java/com/scaler/bookmyshow/controllers/TheatreController.java
+++ src/main/java/com/scaler/bookmyshow/controllers/TheatreController.java
@@ -6,6 +6,8 @@ import com.scaler.bookmyshow.models.SeatType;
 import com.scaler.bookmyshow.models.Theatre;
 import com.scaler.bookmyshow.services.CityService;
 import com.scaler.bookmyshow.services.TheatreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -13,44 +15,42 @@ import java.util.Map;
 
 @Controller
 public class TheatreController {
-    private TheatreService theatreService;
+    private final TheatreService theatreService;
+    private static final Logger logger = LoggerFactory.getLogger(TheatreController.class);
 
     @Autowired
     public TheatreController(TheatreService theatreService) {
         this.theatreService = theatreService;
     }
 
-    public Theatre createTheatre(
-            String name,
-            String address,
-            Long cityId
-    ) {
+    public Theatre createTheatre(String name, String address, Long cityId) {
+        logger.info("Creating theatre with name: {}, address: {}, cityId: {}", name, address, cityId);
         Theatre theatre = null;
         try {
             theatre = this.theatreService.createTheatre(name, address, cityId);
+            logger.info("Theatre created successfully with name: {}", name);
         } catch (CityNotFoundException e) {
-            System.out.println("Something wrong happened");
+            logger.error("CityNotFoundException: City with id {} not found", cityId, e);
         }
-
         return theatre;
     }
 
-    public Theatre addAuditorium(Long theatreId, String name,
-                                 int capacity) {
-        return theatreService.addAuditorium(theatreId, name, capacity);
+    public Theatre addAuditorium(Long theatreId, String name, int capacity) {
+        logger.info("Adding auditorium with name: {}, capacity: {} to theatreId: {}", name, capacity, theatreId);
+        Theatre theatre = theatreService.addAuditorium(theatreId, name, capacity);
+        logger.info("Auditorium added successfully to theatreId: {}", theatreId);
+        return theatre;
     }
 
     /**
-     * In which auditorium
-     * you want to add how many seats
-     * of what type
-     * @param auditoriumId
-     * @param seatCount
+     * Adds seats to an auditorium.
+     *
+     * @param auditoriumId the ID of the auditorium
+     * @param seatCount    the map of seat types and their counts
      */
-    public void addSeats(
-            Long auditoriumId,
-            Map<SeatType, Integer> seatCount
-    ) {
+    public void addSeats(Long auditoriumId, Map<SeatType, Integer> seatCount) {
+        logger.info("Adding seats to auditoriumId: {}, seatCount: {}", auditoriumId, seatCount);
         theatreService.addSeats(auditoriumId, seatCount);
+        logger.info("Seats added successfully to auditoriumId: {}", auditoriumId);
     }
 }
